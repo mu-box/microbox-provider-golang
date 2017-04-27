4fe8e395bf28255c7205b3b8ad80c4ccebc80883
Lyon Hill <lyondhill@gmail.com>
2017-04-27 11:24:49 -0600

    undo

--- provider.go
+++ provider.go
@@ -8,5 +8,6 @@ var backend BackendAdaptor
 // Start the provider using the provided back end
 func Start(b BackendAdaptor) error {
 	backend = b
-	return mux.Run(":8080")
+	mux.Run(":8080")
+	return nil
 }
