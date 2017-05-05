1e443fec37b7845ca71ce43ec80e2e8ae69f0a6f
Lyon Hill <lyondhill@gmail.com>
2017-05-05 15:44:02 -0600

    more

--- provider.go
+++ provider.go
@@ -10,6 +10,7 @@ var backend BackendAdaptor
 // Start the provider using the provided back end
 func Start(b BackendAdaptor, secure bool) error {
 	mux = ace.New()
+	setup()
 	if secure {
 		fmt.Println("secure??")
 		mux.Use(redirect, ace.Logger())
