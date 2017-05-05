269174250a923ab7aa3cc582878bee40f1725698
Lyon Hill <lyondhill@gmail.com>
2017-05-05 15:35:36 -0600

    asdf

--- provider.go
+++ provider.go
@@ -1,6 +1,7 @@
 package provider
 
 import (
+	"fmt"
 )
 
 var backend BackendAdaptor
@@ -8,6 +9,7 @@ var backend BackendAdaptor
 // Start the provider using the provided back end
 func Start(b BackendAdaptor, secure bool) error {
 	if secure {
+		fmt.Println("secure??")
 		mux.Use(redirect)
 	}
 	backend = b
